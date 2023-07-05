694c06ac6052b56faf0230708a5e43de1813af9b
navdeepk037 <21bcs037@nith.ac.in>
2023-07-06 01:24:31 +0530

    map updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var mapButton = document.getElementById('mapButton');
     mapButton.addEventListener('click', function() {
-        var popup = window.open('map.html', '_blank', 'width=800,height=600');
+      initMap();
     });
   });
   
