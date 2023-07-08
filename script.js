f9fcfb6bd9e6872973ad2d4e7b909e4580df5bca
Akanksha080 <21bma017@nith.ac.in>
2023-07-08 22:34:31 +0530

    Finsing route

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var mapButton = document.getElementById('mapButton');
-    mapButton.addEventListener('click', function() {
-      initMap();
-    });
-  });
+
   
